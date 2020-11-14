526a71a24bd1b12b3992ab1445af230b5676fb30
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-11-14 21:28:36 +0000

    feat: Add home and about to urls

--- annotator/urls.py
+++ annotator/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from static_pages.views import home_view, about_view
 
 urlpatterns = [
+    # main pages routing
+    path('', home_view, name='home'),
+    path('home/', home_view, name='home'),
+    path('about/', about_view, name='about'),
+
     path('admin/', admin.site.urls),
 ]
