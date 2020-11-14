0146d8817583914ce8397dcb41dbb13920d76c33
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-11-14 21:41:47 +0000

    feat: Handle upload route functions

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,22 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .forms import UploadFileForm
 from django.shortcuts import render
 
-# Create your views here.
+# Upload view
+def upload_view(request, *args, **kwargs):
+    form = UploadFileForm()
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        files = request.FILES.getlist('file')
+        if form.is_valid():
+            for f in files:
+                handle_uploaded_file(f, f.name)
+            return HttpResponse('Fichiers uplodes avec succes!')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+def handle_uploaded_file(f, name):
+    with open('upload/files_uploaded/' + name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
