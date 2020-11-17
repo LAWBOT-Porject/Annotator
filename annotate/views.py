c05f590563d346b5802a49d2a1f8ef20456a7ab0
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-11-17 22:31:45 +0000

    feat: Add annotate templates and css - first version

--- annotate/views.py
+++ annotate/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 from django.shortcuts import render
 
 def annotate_view(request, *args, **kwargs):
-    return HttpResponse('Annotateur sera bien tot disponible')
+    #return HttpResponse('Annotateur sera bien tot disponible')
+    return render(request, 'annotate.html', {})
