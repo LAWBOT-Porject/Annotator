9a837e42ad74bf4eac25359044327fa8260e13b5
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-11-19 23:25:23 +0000

    feat: Style only the selected file name

--- annotate/static/annotate-app.js
+++ annotate/static/annotate-app.js
@@ -1,16 +1,30 @@
+/* Author : Sid Ali MAHMOUDI @ 20/11/2020 */
+
 //Array of list items
 const li_tags = document.querySelectorAll('li.file-link');
+
+// Paragraph where to put decision file content
 const p_decision = document.getElementById('file-contents');
 
 const displyTextFile = (evt) => {
-    let link = evt.target.getAttribute("data-link");
-    evt.target.style.color = 'green';
-    fetch(link)
-    .then(response => response.text())
-    .then(data => {
-      p_decision.innerText = data;
-    });
+  // Get selected decision file link
+  let link = evt.target.getAttribute("data-link");
+  // Style the selected file
+  evt.target.classList.add('selected_file');
+  // Loop over other li items to unstyle all others (unstyle previously styled items)
+  li_tags.forEach(li => {
+    if(li !== evt.target) {
+      li.classList.remove('selected_file');
+    }
+  });
+  // Send GET request to server which will handle the requested file url (with file_read function)
+  fetch(link)
+  .then(response => response.text())
+  .then(data => {
+    // Populate the decision file content to the appropriate p tag
+    p_decision.innerText = data;
+  });  
 }
 
+// Attach every li with displayTextFile function
 li_tags.forEach( li => li.addEventListener('click', displyTextFile));
-console.log('3');
