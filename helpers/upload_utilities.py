2e816fda5c324db1f520437f8a3effd8ea89fed9
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-12-07 19:32:35 +0000

    core: Add helper functions in serprate folder

---
+++ helpers/upload_utilities.py
@@ -0,0 +1,8 @@
+"""Upload helper functions
+
+This package contains utility functions for uploading files into the server
+
+This file can also be imported as a module and contains the following functions:
+
+    * handle_uploaded_file - save the uploaded files
+"""
