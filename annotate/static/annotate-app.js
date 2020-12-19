7b039b20a70a58008116dfe37e7e09d5c8978c39
MahmoudAliEng <eng.sidali.mahmoudi@gmail.com>
2020-12-19 15:47:07 +0000

    fix: Increase the input font size and restyle shape

--- annotate/static/annotate-app.js
+++ annotate/static/annotate-app.js
@@ -1,36 +1,36 @@
 /* Author : Sid Ali MAHMOUDI @ 20/11/2020 */
 
 // Array of list items
-const li_tags = document.querySelectorAll('li.file-link');
+const li_tags = document.querySelectorAll("li.file-link");
 
 // File index span
-const file_index = document.getElementById('file-index');
+const file_index = document.getElementById("file-index");
 // Previous icon
-const previous = document.querySelector('.previous');
+const previous = document.querySelector(".previous");
 // Next icon
-const next = document.querySelector('.next');
+const next = document.querySelector(".next");
 // Index of the current file
 let index = -1;
 
 // Paragraph where to put decision file content
-const p_decision = document.getElementById('file-contents');
+const p_decision = document.getElementById("file-contents");
 // Zoom out button
-const zoom_out_btn = document.getElementsByClassName('zoom-out');
+const zoom_out_btn = document.getElementsByClassName("zoom-out");
 // Zoom in button
-const zoom_in_btn = document.getElementsByClassName('zoom-in');
+const zoom_in_btn = document.getElementsByClassName("zoom-in");
 // Text size input field
-const text_size = document.getElementById('text-size');
+const text_size = document.getElementById("text-size");
 
 const goToNext = () => {
   index = (index + 1) % lis.length;
   lis[index].click();
-}
+};
 
 const goToPrevious = () => {
   index = (index - 1) % lis.length;
-  if (index < 0 ) index = lis.length -1;
+  if (index < 0) index = lis.length - 1;
   lis[index].click();
-}
+};
 
 const displyTextFile = (evt) => {
   // Change the index of the current file in the tool pane
@@ -38,50 +38,50 @@ const displyTextFile = (evt) => {
   // Get selected decision file link
   let link = evt.target.getAttribute("data-link");
   // Style the selected file
-  evt.target.classList.add('selected_file');
+  evt.target.classList.add("selected_file");
   // Loop over other li items to unstyle all others (unstyle previously styled items)
-  li_tags.forEach(li => {
-    if(li !== evt.target) {
-      li.classList.remove('selected_file');
+  li_tags.forEach((li) => {
+    if (li !== evt.target) {
+      li.classList.remove("selected_file");
     }
   });
   // Send GET request to server which will handle the requested file url (with file_read function)
   fetch(link)
-  .then(response => response.text())
-  .then(data => {
-    // Populate the decision file content to the appropriate p tag
-    p_decision.innerText = data;
-    p_decision.style.fontSize = "20px";
-  });  
-}
+    .then((response) => response.text())
+    .then((data) => {
+      // Populate the decision file content to the appropriate p tag
+      p_decision.innerText = data;
+      p_decision.style.fontSize = "20px";
+    });
+};
 
 const zoom_out = () => {
-  let a = parseInt(p_decision.style.fontSize.split('p')[0]) -1;
-  p_decision.style.fontSize = String(a) + 'px';
-}
+  let a = parseInt(p_decision.style.fontSize.split("p")[0]) - 1;
+  p_decision.style.fontSize = String(a) + "px";
+};
 const zoom_in = () => {
-  let a = parseInt(p_decision.style.fontSize.split('p')[0]) +1;
-  p_decision.style.fontSize = String(a) + 'px';
-}
+  let a = parseInt(p_decision.style.fontSize.split("p")[0]) + 1;
+  p_decision.style.fontSize = String(a) + "px";
+};
 const set_size = (evt) => {
   let value = evt.target.value;
-  if(value < 0) {
+  if (value < 0) {
     value = -value;
     evt.target.value = value;
   }
-  p_decision.style.fontSize = String(value) + 'px';
-}
+  p_decision.style.fontSize = String(value) + "px";
+};
 
-text_size.addEventListener('keyup', set_size);
-text_size.addEventListener('change', set_size);
+text_size.addEventListener("keyup", set_size);
+text_size.addEventListener("change", set_size);
 
 // Convert Node list (of li) to an Array
-lis = [...li_tags]
+lis = [...li_tags];
 // Construct the lis content array
-let lis_content = []
-lis.forEach( li => lis_content.push(li.innerHTML));
+let lis_content = [];
+lis.forEach((li) => lis_content.push(li.innerHTML));
 
 // Attach every li with displayTextFile function
-li_tags.forEach( li => li.addEventListener('click', displyTextFile));
-previous.addEventListener('click', goToPrevious);
-next.addEventListener('click', goToNext);
+li_tags.forEach((li) => li.addEventListener("click", displyTextFile));
+previous.addEventListener("click", goToPrevious);
+next.addEventListener("click", goToNext);
