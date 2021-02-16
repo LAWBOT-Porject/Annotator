e4f76b33191359578276a23b43c330483b1fa81e
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2021-02-16 03:09:08 +0100

    feat: Create category and associate it to normes

--- categorize/urls.py
+++ categorize/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import categorize_view, create_norme
+from .views import categorize_view, create_norme, create_category
 
 app_name = 'categorize'
 urlpatterns = [
     path('', categorize_view, name='categorize_view'),
     path('create-norme', create_norme, name='create_norme'),
+    path('create-category', create_category, name='create_category'),
 ]
--- categorize/views.py
+++ categorize/views.py
@@ -28,24 +28,27 @@ def categorize_view(request):
         temp.append( getattr(all_normes[i], 'texte_norme'))
         temp.append( getattr(all_normes[i], 'date_debut'))
         temp.append( getattr(all_normes[i], 'date_fin'))
+        temp.append( getattr(all_normes[i], 'id'))
         normes.append(temp)
     return render(request, 'categorize.html', {"categories" : categories, "normes" : normes})
 
 def create_norme(request):
     if request.method == 'POST':
-        print(request.POST.get('new-fondement'))#.decode('utf-8'))
-        print(request.POST.get('new-descriptif'))#.decode('utf-8'))
-        print(request.POST.get('new-date-debut'))
-        print(request.POST.get('new-date-fin'))
         new_norme = Norme.objects.create(fondement=request.POST.get('new-fondement'),
                             texte_norme=request.POST.get('new-descriptif'),
                             date_debut=request.POST.get('new-date-debut'),
                             date_fin=request.POST.get('new-date-fin'),)
-        new_norme_id = getattr(new_norme, 'id')
-        print(new_norme_id)
         related_categories = request.POST.getlist('related-categ')
-        print(related_categories)
         for item in related_categories:
             item = Categorie.objects.get(noppac=item)
-            new_relation = CategorieNorme.objects.create(categorie_id=item, norme_id=new_norme)
+            CategorieNorme.objects.create(categorie_id=item, norme_id=new_norme)
+    return categorize_view(request)
+
+def create_category(request):
+    if request.method == 'POST':
+        new_categorie = Categorie.objects.create(noppac= request.POST.get('new-noppac'),
+                                                description=request.POST.get('new-description'),
+                                                objet=request.POST.get('new-objet'))
+        for id_ in request.POST.getlist('related-norme'):
+            CategorieNorme.objects.create(categorie_id=new_categorie, norme_id=Norme.objects.get(id=id_))
     return categorize_view(request)
